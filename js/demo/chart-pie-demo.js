7b427c85ea02bfbbb6e6cf4db81ac1e31816a4a1
James <5000win@naver.com>
2022-09-17 16:00:28 +0900

    영상 소스 반영

--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -4,80 +4,80 @@
 Chart.defaults.global.defaultFontColor = "#858796";
 
 // Pie Chart Example
-var ctx = document.getElementById("myPieChart");
-var myPieChart = new Chart(ctx, {
-  type: "doughnut",
-  data: {
-    labels: ["생산", "유틸리티", "사무", "기타"],
-    datasets: [
-      {
-        data: [70, 27, 2, 1],
-        backgroundColor: ["#4e73df", "#1cc88a", "#36b9cc", "#f6c23e"],
-        hoverBackgroundColor: ["#2e59d9", "#17a673", "#2c9faf", "#f6c23e"],
-        hoverBorderColor: "rgba(234, 236, 244, 1)",
-      },
-    ],
-  },
-  options: {
-    title: {
-      display: true,
-      text: "총 에너지사용량",
-    },
-    maintainAspectRatio: false,
-    tooltips: {
-      backgroundColor: "rgb(255,255,255)",
-      bodyFontColor: "#858796",
-      borderColor: "#dddfeb",
-      borderWidth: 1,
-      xPadding: 15,
-      yPadding: 15,
-      displayColors: false,
-      caretPadding: 10,
-    },
-    legend: {
-      display: false,
-    },
-    cutoutPercentage: 80,
-  },
-});
+// var ctx = document.getElementById("myPieChart");
+// var myPieChart = new Chart(ctx, {
+//   type: "doughnut",
+//   data: {
+//     labels: ["생산", "유틸리티", "사무", "기타"],
+//     datasets: [
+//       {
+//         data: [70, 27, 2, 1],
+//         backgroundColor: ["#4e73df", "#1cc88a", "#36b9cc", "#f6c23e"],
+//         hoverBackgroundColor: ["#2e59d9", "#17a673", "#2c9faf", "#f6c23e"],
+//         hoverBorderColor: "rgba(234, 236, 244, 1)",
+//       },
+//     ],
+//   },
+//   options: {
+//     title: {
+//       display: true,
+//       text: "총 에너지사용량",
+//     },
+//     maintainAspectRatio: false,
+//     tooltips: {
+//       backgroundColor: "rgb(255,255,255)",
+//       bodyFontColor: "#858796",
+//       borderColor: "#dddfeb",
+//       borderWidth: 1,
+//       xPadding: 15,
+//       yPadding: 15,
+//       displayColors: false,
+//       caretPadding: 10,
+//     },
+//     legend: {
+//       display: false,
+//     },
+//     cutoutPercentage: 80,
+//   },
+// });
 
 // Pie Chart Example
-var ctx2 = document.getElementById("myPieChart2");
-var myPieChart2 = new Chart(ctx2, {
-  type: "doughnut",
-  data: {
-    labels: ["생산", "유틸리티", "사무", "기타"],
-    datasets: [
-      {
-        data: [45, 12, 13, 20],
-        backgroundColor: ["#4e73df", "#1cc88a", "#36b9cc", "#f6c23e"],
-        hoverBackgroundColor: ["#2e59d9", "#17a673", "#2c9faf", "#f6c23e"],
-        hoverBorderColor: "rgba(234, 236, 244, 1)",
-      },
-    ],
-  },
-  options: {
-    title: {
-      display: true,
-      text: "총 에너지비용",
-    },
-    maintainAspectRatio: false,
-    tooltips: {
-      backgroundColor: "rgb(255,255,255)",
-      bodyFontColor: "#858796",
-      borderColor: "#dddfeb",
-      borderWidth: 1,
-      xPadding: 15,
-      yPadding: 15,
-      displayColors: false,
-      caretPadding: 10,
-    },
-    legend: {
-      display: false,
-    },
-    cutoutPercentage: 80,
-  },
-});
+// var ctx2 = document.getElementById("myPieChart2");
+// var myPieChart2 = new Chart(ctx2, {
+//   type: "doughnut",
+//   data: {
+//     labels: ["생산", "유틸리티", "사무", "기타"],
+//     datasets: [
+//       {
+//         data: [45, 12, 13, 20],
+//         backgroundColor: ["#4e73df", "#1cc88a", "#36b9cc", "#f6c23e"],
+//         hoverBackgroundColor: ["#2e59d9", "#17a673", "#2c9faf", "#f6c23e"],
+//         hoverBorderColor: "rgba(234, 236, 244, 1)",
+//       },
+//     ],
+//   },
+//   options: {
+//     title: {
+//       display: true,
+//       text: "총 에너지비용",
+//     },
+//     maintainAspectRatio: false,
+//     tooltips: {
+//       backgroundColor: "rgb(255,255,255)",
+//       bodyFontColor: "#858796",
+//       borderColor: "#dddfeb",
+//       borderWidth: 1,
+//       xPadding: 15,
+//       yPadding: 15,
+//       displayColors: false,
+//       caretPadding: 10,
+//     },
+//     legend: {
+//       display: false,
+//     },
+//     cutoutPercentage: 80,
+//   },
+// });
 
 // Pie Chart Example
 var ctx3 = document.getElementById("myPieChart3");
